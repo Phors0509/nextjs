81b46b2b5b7c51cff518fa54b74a53cc9cd1bd98
PhorsBeatRmx <phorsbeatrmx0509@gmail.com>
2024-08-20 16:03:18 -1200

    modified tt

--- src/components/requiredTest/requiredTest.tsx
+++ src/components/requiredTest/requiredTest.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+// import { useForm } from "react-hook-form";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import * as yup from "yup";
 
 // const schema = yup.object().shape({
 //     email: yup
