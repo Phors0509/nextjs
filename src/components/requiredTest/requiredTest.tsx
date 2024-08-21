ffb8d857e94615fb4a4226138a218f7b96e1a2f6
PhorsBeatRmx <phorsbeatrmx0509@gmail.com>
2024-08-20 18:39:17 -1200

    modified test

--- src/components/requiredTest/requiredTest.tsx
+++ src/components/requiredTest/requiredTest.tsx
@@ -1,40 +1,40 @@
 "use client";
 import React from "react";
-// import { useForm } from "react-hook-form";
-// import { yupResolver } from "@hookform/resolvers/yup";
-// import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-// const schema = yup.object().shape({
-//     email: yup
-//         .string()
-//         .email("Invalid email format")
-//         .required("Email is required"),
-//     password: yup
-//         .string()
-//         .min(6, "Password must be at least 6 characters")
-//         .required("Password is required"),
-// });
+const schema = yup.object().shape({
+    email: yup
+        .string()
+        .email("Invalid email format")
+        .required("Email is required"),
+    password: yup
+        .string()
+        .min(6, "Password must be at least 6 characters")
+        .required("Password is required"),
+});
 
 const RequiredTest = () => {
-    // const {
-    //     register,
-    //     handleSubmit,
-    //     formState: { errors },
-    // } = useForm({
-    //     resolver: yupResolver(schema),
-    // });
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
 
-    // const onSubmit = (data: any) => {
-    //     console.log(data);
-    // };
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
 
     return (
-        <form>
-            {/* <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
                 <input {...register("email")} placeholder="email" />
                 <p>{errors.email?.message}</p>
-            </div> */}
-            {/* <div>
+            </div>
+            <div>
                 <input
                     type="password"
                     {...register("password")}
@@ -42,7 +42,7 @@ const RequiredTest = () => {
                 />
                 <p>{errors.password?.message}</p>
             </div>
-            <button type="submit">Submit</button> */}
+            <button type="submit">Submit</button>
         </form>
     );
 };
