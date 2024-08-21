8f9cbe7776fa7f8149dc16233a9c4a36c0171efe
PhorsBeatRmx <phorsbeatrmx0509@gmail.com>
2024-08-20 20:34:50 -1200

    modified test15

--- src/components/requiredTest/requiredTest.tsx
+++ src/components/requiredTest/requiredTest.tsx
@@ -1,35 +1,36 @@
 "use client";
 import React from "react";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+// import { useForm } from "react-hook-form";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import * as yup from "yup";
 
-const schema = yup.object().shape({
-    email: yup
-        .string()
-        .email("Invalid email format")
-        .required("Email is required"),
-    password: yup
-        .string()
-        .min(6, "Password must be at least 6 characters")
-        .required("Password is required"),
-});
+// const schema = yup.object().shape({
+//     email: yup
+//         .string()
+//         .email("Invalid email format")
+//         .required("Email is required"),
+//     password: yup
+//         .string()
+//         .min(6, "Password must be at least 6 characters")
+//         .required("Password is required"),
+// });
 
 const RequiredTest = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        resolver: yupResolver(schema),
-    });
+    // const {
+    //     register,
+    //     handleSubmit,
+    //     formState: { errors },
+    // } = useForm({
+    //     resolver: yupResolver(schema),
+    // });
 
-    const onSubmit = (data: any) => {
-        console.log(data);
-    };
+    // const onSubmit = (data: any) => {
+    //     console.log(data);
+    // };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <>
+            {/* <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <input {...register("email")} placeholder="email" />
                 <p>{errors.email?.message}</p>
@@ -39,11 +40,12 @@ const RequiredTest = () => {
                     type="password"
                     {...register("password")}
                     placeholder="password"
-                />
+                    />
                 <p>{errors.password?.message}</p>
             </div>
             <button type="submit">Submit</button>
-        </form>
+        </form> */}
+        </>
     );
 };
 
